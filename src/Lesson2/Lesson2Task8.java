940a21cf1578a081a52bf7b28e25c636995afbf0
gentletempter <vitamin_bard@mail.ru>
2021-08-05 16:32:50 +0300

    Complete

--- src/Lesson2/Lesson2Task8.java
+++ src/Lesson2/Lesson2Task8.java
@@ -6,9 +6,19 @@ public class Lesson2Task8 {
     public static void main(String[] args) {
         char[][] array = new char[4][4];
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = i >=j ? '*' : ' ';
+            for (int j = array[i].length - 1; j >= 0; j--) {
+                array[i][j] = i >= j ? '*' : ' ';
+                System.out.print(array[i][j] + " ");
             }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = i >= j ? '*' : ' ';
+                System.out.print(array[j][i] + " ");
+            }
+            System.out.println();
         }
     }
 }
