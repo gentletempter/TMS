11245125e526de8e87c578d02f82349d9578dc1c
gentletempter <vitamin_bard@mail.ru>
2021-08-06 16:22:01 +0300

    Done

---
+++ src/NewLesson1/NewLesson1Task1.java
@@ -0,0 +1,7 @@
+package NewLesson1;
+
+public class NewLesson1Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, team");
+    }
+}
---
+++ src/NewLesson1/NewLesson1Task2.java
@@ -0,0 +1,11 @@
+package NewLesson1;
+
+import java.util.Scanner;
+
+public class NewLesson1Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name here: ");
+        System.out.print("Hello, " + sc.nextLine());
+    }
+}
